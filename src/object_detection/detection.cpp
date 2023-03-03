9c608ed36a2b8a88a5c48a15f5154f2d26050954
Rahul <rahulsud48@gmail.com>
2023-03-03 19:35:20 +0530

    stereo depth solution implemented and working

--- src/object_detection/detection.cpp
+++ src/object_detection/detection.cpp
@@ -31,7 +31,7 @@ void ssd_detector::display_text(cv::Mat& img, std::string text, int x, int y)
     rectangle(img, Point(x,y-textSize.height-baseLine), Point(x+textSize.width,y+baseLine),
              Scalar(0,0,0),-1);
     // Display text inside the rectangle
-    putText(img, text, Point(x,y-5), FONT_HERSHEY_SIMPLEX, 0.7, Scalar(0,255,255), 1, LINE_AA);
+    putText(img, text, Point(x,y-5), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0,255,255), 1, LINE_AA);
 }
 
 void ssd_detector::display_objects(Mat& img, Mat objects, cv::Mat& depth_map ,float threshold = 0.25)
@@ -45,15 +45,35 @@ void ssd_detector::display_objects(Mat& img, Mat objects, cv::Mat& depth_map ,fl
         int y = static_cast<int>(objects.at<float>(i, 4) * img.rows);
         int w = static_cast<int>(objects.at<float>(i, 5) * img.cols - x);
         int h = static_cast<int>(objects.at<float>(i, 6) * img.rows - y);
-
-        int x_mid = x + (w/2);
-        int y_mid = y + (h/2);
-        float depth = depth_map.at<float>(x_mid, y_mid);
+        // try {
+        // // Block of code to try
+        //     depth = depth_map.at<float>(x_mid, y_mid);
+        //     throw 505;
+        //     // throw exception; // Throw an exception when a problem arise
+        // }
+        // catch (...){
+        // // Block of code to handle errors
+        //     depth = -1;
+        // }
         // Check if the detection is of good quality
         if (score > threshold){
+            int x_mid = static_cast<int>(x + (w/2));
+            int y_mid = static_cast<int>(y + (h/2));
+            // float depth;
+            cv::Rect myROI(x_mid, y_mid, 10, 10);
+            cv::Mat depth_object = depth_map(myROI);
+            double Min,Max;
+            cv::minMaxLoc(depth_object,&Min,&Max);
             // display_text(img, classes[classId].c_str(), x, y);
-            std::cout<<"the depth of: "<< classes[classId].c_str() << "is: "<< depth<<std::endl;
-            display_text(img, classes[classId].c_str(), x_mid, y_mid);
+            std::cout<<"the depth of: "<< classes[classId].c_str() << " is: "<< Min<<std::endl;
+
+            std::ostringstream ss;
+            ss << Min;
+            std::string s(ss.str());
+
+
+            // display_text(img, classes[classId].c_str(), x, y);
+            display_text(img, s, x_mid, y_mid);
             rectangle(img, Point(x,y), Point(x+w, y+h), Scalar(255,255,255), 2);
         }
     }
